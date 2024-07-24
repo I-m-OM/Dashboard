39ae9e84c4eed953c32e720f35b807180c884e76
Om Chaudhary <im.om.chaudhary@gmail.com>
2024-07-24 23:26:08 +0530

    removed imports

--- src/App.js
+++ src/App.js
@@ -2,12 +2,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./Home";
 import Leetcode from "./Pages/Leetcode";
-import Statslc from "./Pages/Leet/Statslc";
-import Problemslc from "./Pages/Leet/Problemslc";
-import Contestlc from "./Pages/Leet/Contestlc";
-import Statscf from "./Pages/Code/Statscf";
-import Problemscf from "./Pages/Code/Problemscf";
-import Contestcf from "./Pages/Code/Contestcf";
 import Codeforces from "./Pages/Codeforces";
 import React from "react";
 
@@ -16,16 +10,8 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/Leetcode" element={<Leetcode />}>
-          {/* <Route path="" element={<Statslc />} />
-          <Route path="problems" element={<Problemslc />} />
-          <Route path="contests" element={<Contestlc />} /> */}
-        </Route>
-        <Route path="/Codeforces" element={<Codeforces />}>
-          {/* <Route path="" element={<Statscf />} />
-          <Route path="problems" element={<Problemscf />} />
-          <Route path="contests" element={<Contestcf />} /> */}
-        </Route>
+        <Route path="/Leetcode" element={<Leetcode />}></Route>
+        <Route path="/Codeforces" element={<Codeforces />}></Route>
       </Routes>
     </BrowserRouter>
   );
